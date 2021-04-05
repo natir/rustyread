91f5cf02fe23fc3c62cc4fadfffc260f06c318fe
natir <natir@users.noreply.github.com>
2021-04-05 15:13:46 +0000

    deploy: 6a611fc3bf38f13d7ab86b2602ffe16468b60954

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["rustyread"] = {"name":"","dirs":[{"name":"cli","files":["mod.rs","simulate.rs"]},{"name":"error","files":["cli.rs","mod.rs","model.rs"]},{"name":"model","files":["error.rs","identity.rs","length.rs","mod.rs","quality.rs"]},{"name":"simulate","files":["mod.rs"]}],"files":["alignment.rs","lib.rs","references.rs"]};
+sourcesIndex["rustyread"] = {"name":"","dirs":[{"name":"cli","files":["mod.rs","simulate.rs"]},{"name":"error","files":["cli.rs","mod.rs","model.rs"]},{"name":"model","files":["error.rs","identity.rs","length.rs","mod.rs","quality.rs"]},{"name":"simulate","files":["description.rs","mod.rs"]}],"files":["alignment.rs","lib.rs","references.rs"]};
 createSourceSidebar();
