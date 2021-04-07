060cc5d15b1a46601459f3e64161ebe3e9273386
Pierre Marijon <pierre.marijon@hhu.de>
2021-04-07 10:52:13 +0200

    Add an align information that return edit distance and extended cigar string

--- src/alignment.rs
+++ src/alignment.rs
@@ -10,6 +10,33 @@ pub fn edit_distance(seq1: &[u8], seq2: &[u8]) -> u64 {
     bio::alignment::distance::levenshtein(seq1, seq2) as u64
 }
 
-pub fn identity(perfect: &[u8], error: &[u8]) -> f64 {
-    edit_distance(perfect, error) as f64 / perfect.len() as f64
+pub fn align(raw: &[u8], err: &[u8]) -> (usize, Box<[u8]>) {
+    let mut aligner = bio::alignment::pairwise::Aligner::with_capacity(
+        err.len(),
+        raw.len(),
+        0,
+        0,
+        |a: u8, b: u8| if a == b { 1i32 } else { 0i32 },
+    );
+    //let mut aligner = bio::alignment::pairwise::banded::Aligner::with_capacity(err.len(), raw.len(), 0, 0, |a: u8, b: u8| if a == b { 1i32 } else { 0i32 }, 7, 5);
+
+    let alignment = aligner.global(raw, err);
+
+    let mut cigar = Vec::with_capacity(raw.len().max(err.len()));
+
+    for op in alignment.operations {
+        match op {
+            bio::alignment::AlignmentOperation::Xclip(len) => cigar.extend(vec![b'X'; len]),
+            bio::alignment::AlignmentOperation::Yclip(len) => cigar.extend(vec![b'X'; len]),
+            bio::alignment::AlignmentOperation::Subst => cigar.push(b'X'),
+            bio::alignment::AlignmentOperation::Ins => cigar.push(b'I'),
+            bio::alignment::AlignmentOperation::Del => cigar.push(b'D'),
+            bio::alignment::AlignmentOperation::Match => cigar.push(b'='),
+        }
+    }
+
+    (
+        cigar.iter().filter(|x| **x != b'=').count(),
+        cigar.into_boxed_slice(),
+    )
 }
